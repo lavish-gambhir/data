d283b3a807f9afd46aeed8f49f2f05c756d0a58c
Lavish Gambhir <lavishgambhir@Lavishs-MacBook-Air.local>
2019-02-03 16:27:50 +0530

    Implementation of Stack ADT Using List as an underlying structure

---
+++ stack/stack01.py
@@ -0,0 +1,21 @@
+# temp stack implementation
+
+class Stack(object):
+	def __init__(self):
+		self.items = []
+
+
+	def push(self, item):
+		self.items.append(item)
+
+
+	def pop(self):
+		return self.items.pop()
+
+
+	def peek(self):
+		return self.items[len(self.items)-1]
+
+
+	def is_empty(self):
+		return self.items == []
