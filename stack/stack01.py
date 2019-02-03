5f5143370b2f20e8e69bab1e1ad025f03f611fb2
Lavish Gambhir <lavishgambhir@Lavishs-MacBook-Air.local>
2019-02-03 18:32:49 +0530

    Changed list var to private.

--- stack/stack01.py
+++ stack/stack01.py
@@ -2,20 +2,20 @@
 
 class Stack(object):
 	def __init__(self):
-		self.items = []
+		self._items = []
 
 
 	def push(self, item):
-		self.items.append(item)
+		self._items.append(item)
 
 
 	def pop(self):
-		return self.items.pop()
+		return self._items.pop()
 
 
 	def peek(self):
-		return self.items[len(self.items)-1]
+		return self._items[len(self.items)-1]
 
 
 	def is_empty(self):
-		return self.items == []
+		return self._items == []
