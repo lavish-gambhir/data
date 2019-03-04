e44a573c3f823ebed6af05d9ebc48d442465c93a
Lavish Gambhir <lavishgambhir@Lavishs-MacBook-Air.local>
2019-03-05 05:18:38 +0530

    Added depth and height routines in Tree ADT.

--- tree/tree.py
+++ tree/tree.py
@@ -51,3 +51,25 @@ class Tree(object):
     def is_empty(self):
         """Return True if the tree is empty."""
         return len(self) == 0
+
+    def depth(self, p):
+        """Return the number of levels seperating Position p from the root."""
+        if self.is_root(p):
+            return 0
+        return 1 + self.depth(self.parent(p))
+
+    
+    def _height(self, p):
+        """Return the height of the subtree rooted at Position p."""
+        if self.is_leaf(p):
+            return 0
+        return 1 + max(self._height(c) for c in self.children(p))
+
+    
+    def height(self, p=None):
+        """Return the height of the subtree rooted at Position p.
+        if p is None, return the height of the entire tree."""
+        
+        if p is None:
+            p = self.root()
+        return self._height(p)
